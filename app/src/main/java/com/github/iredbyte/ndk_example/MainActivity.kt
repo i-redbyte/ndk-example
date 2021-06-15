1335e7e1abba9669e9bad206cac21ce6a905ee76
Ilya Sokolov <developer.sokolov@gmail.com>
2021-06-16 00:57:23 +0300

    change simple example

--- app/src/main/java/com/github/iredbyte/ndk_example/MainActivity.kt
+++ app/src/main/java/com/github/iredbyte/ndk_example/MainActivity.kt
@@ -6,19 +6,17 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    init {
+        System.loadLibrary("hellojni")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val tvHello = findViewById<TextView>(R.id.text_view_hello_neon)
-        tvHello.setText(stringFromJNI())
+        tvHello.text = stringFromJNI()
     }
 
     external fun stringFromJNI(): String?
-
-
-        init {
-            System.loadLibrary("hello-neon")
-        }
-
 
 }
